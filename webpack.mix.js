cfb1cfc7c2da21be0e0ee623ca1f0becaf487cd8
Kaue Oliveira Almeida <ueak21@gmail.com>
2019-04-09 17:54:59 -0300

    configurações do front-end e tema

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js');
+   //.sass('resources/sass/app.scss', 'public/css');
+
+// Theme
+mix.js('vendor/uspdev/laravel-comet-theme/resources/assets/js/script.js', 'public/js')
+  .sass('vendor/uspdev/laravel-comet-theme/resources/assets/sass/app.scss', 'public/css');
